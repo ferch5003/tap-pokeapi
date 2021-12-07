f0134d40dd2c6d7f80e5a45e07780e4b2f3a6b30
Fernando Visbal <ferch5003@gmail.com>
2021-12-07 14:00:08 -0500

    feat: Added do discover fucntion

--- tap_pokeapi/__init__.py
+++ tap_pokeapi/__init__.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
+import json
 import singer
-from singer import utils, metadata
+from singer import utils
+import sys
 from tap_pokeapi.discover import discover
 from tap_pokeapi.sync import sync
 
 REQUIRED_CONFIG_KEYS = []
 LOGGER = singer.get_logger()
+
+
+def do_discover():
+    LOGGER.info('Starting discover')
+    catalog = discover()
+    json.dump(catalog.to_dict(), sys.stdout, indent=2)
+    LOGGER.info('Finished discover')
 
 
 @utils.handle_top_exception(LOGGER)
@@ -15,14 +24,13 @@ def main():
 
     # If discover flag was passed, run discovery mode and dump output to stdout
     if args.discover:
-        catalog = discover()
-        catalog.dump()
+        do_discover()
     # Otherwise run in sync mode
     else:
         if args.catalog:
             catalog = args.catalog
         else:
-            catalog = discover()
+            do_discover()
         sync(args.config, args.state, catalog)
 
 
