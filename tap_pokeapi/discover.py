b06d9b46ebff6bb9a8d0af70c968c89ade7a50cd
Fernando Visbal <ferch5003@gmail.com>
2021-12-07 13:29:44 -0500

    conf: Mod functions on project

---
+++ tap_pokeapi/discover.py
@@ -0,0 +1,28 @@
+from singer.catalog import Catalog, CatalogEntry
+from tap_pokeapi.schema import load_schemas
+
+
+def discover():
+    raw_schemas = load_schemas()
+    streams = []
+    for stream_id, schema in raw_schemas.items():
+        # TODO: populate any metadata and stream's key properties here..
+        stream_metadata = []
+        key_properties = []
+        streams.append(
+            CatalogEntry(
+                tap_stream_id=stream_id,
+                stream=stream_id,
+                schema=schema,
+                key_properties=key_properties,
+                metadata=stream_metadata,
+                replication_key=None,
+                is_view=None,
+                database=None,
+                table=None,
+                row_count=None,
+                stream_alias=None,
+                replication_method=None,
+            )
+        )
+    return Catalog(streams)
---
+++ tap_pokeapi/schema.py
@@ -0,0 +1,25 @@
+import os
+import json
+from singer.schema import Schema
+
+# Reference:
+# https://github.com/singer-io/getting-started/blob/master/docs/DISCOVERY_MODE.md#Metadata
+STREAMS = {
+    'pokemons': {
+    }
+}
+
+
+def get_abs_path(path):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+
+
+def load_schemas():
+    """ Load schemas from schemas folder """
+    schemas = {}
+    for filename in os.listdir(get_abs_path('schemas')):
+        path = get_abs_path('schemas') + '/' + filename
+        file_raw = filename.replace('.json', '')
+        with open(path) as file:
+            schemas[file_raw] = Schema.from_dict(json.load(file))
+    return schemas
